a843b307a244f4b5b0a92157caf8739472f08f49
Olivier Riccini <olivier-webdev@hotmail.com>
2018-02-23 10:51:11 -0500

    Prepare for Heroku

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,12 +1,4 @@
 class DosesController < ApplicationController
-  def index
-    @doses = Dose.all
-  end
-
-  def show
-    @doses = Do
-  end
-
   def new
     @dose = Dose.new
     @cocktail = Cocktail.find(params[:cocktail_id])
@@ -14,22 +6,27 @@ class DosesController < ApplicationController
 
   def create
     @dose = Dose.new(dose_params)
-    @dose.cocktail_id = params[:cocktail_id]
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+
     if @dose.save
-      redirect_to cocktail_path(@dose.cocktail), notice: 'Dose was successfully created.'
+      redirect_to cocktail_path(@cocktail)
     else
-      render :new, notice: "your dose wasn't saved."
+      render :new
     end
   end
 
   def destroy
     @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
     @dose.destroy
+
+    redirect_to cocktail_path(@cocktail)
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:amount, :ingredient_id, :description, :cocktail_id)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
